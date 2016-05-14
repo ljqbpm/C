79bbf2cccb27e44eba340d8005aa1bdc6cfe509f
Benjamin.Field <a760@a760demacbook-air.local>
2016-05-15 00:21:32 +0800

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by Benjamin.Field on 16/5/15.
+//  Copyright © 2016年 Benjamin.Field. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
